a77748199d3db9b23de3d5ac617455814112d0de
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-08 09:52:41 -0600

    clusterer is working

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
         jshint: {
             files: ["assets/js/**/*.js"],
             options: {
-                predef: ["document", "console", "alert", "angular", "firebase"],
+                predef: ["document", "console", "alert", "angular", "firebase", "google", "MarkerClusterer"],
                 esnext: true,
                 globalstrict: true,
                 globals: {},
--- assets/js/ctrl/map.js
+++ assets/js/ctrl/map.js
@@ -1,12 +1,27 @@
 "use strict";
 
-angular.module("inkmap").controller("MapCtrl", function($scope, artists, NgMap, MAPS) {
+require("../../../dist/markerclusterer.js");
+
+angular.module("inkmap").controller("MapCtrl", function ($scope, artists, NgMap, MAPS) {
     $scope.artists = artists;
     $scope.mapScript = `http://maps.google.com/maps/api/js?key=${MAPS.apiKey}`;
 
-    NgMap.getMap().then(function(map) {
-      console.log(map.getCenter());
-      console.log('markers', map.markers);
-      console.log('shapes', map.shapes);
+    // $scope.artists => coords
+    let markers = Object.entries(artists).map(a => [a[1].loc.lat, a[1].loc.lng]);
+    markers = markers.map(a => {
+        a = { position: a };
+        return a;
+    });
+
+    MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = 'assets/img/m';
+
+    let dynMarkers = [];
+    NgMap.getMap().then(function (map) {
+        map.markers = [];
+        for (let marker of markers) {
+            var latLng = new google.maps.LatLng(marker.position[0], marker.position[1]);
+            dynMarkers.push(new google.maps.Marker({ position: latLng }));
+        }
+        $scope.markerClusterer = new MarkerClusterer(map, dynMarkers, {});
     });
 });
