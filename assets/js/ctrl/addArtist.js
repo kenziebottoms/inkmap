7108e29de10d018a0c567357591adb498f479b3a
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-09 09:37:26 -0600

    autocomplete search works, returns lat/lng

--- assets/js/ctrl/addArtist.js
+++ assets/js/ctrl/addArtist.js
@@ -1,5 +1,18 @@
 "use strict";
 
-angular.module("inkmap").controller("AddArtistCtrl", function($scope) {
-    
+angular.module("inkmap").controller("AddArtistCtrl", function ($scope, GOOGLE) {
+    $scope.mapScript = `http://maps.google.com/maps/api/js?key=${GOOGLE.apiKey}&libraries=places&callback=initialize`;
+
+    let autocomplete;
+    $scope.initialize = () => {
+        autocomplete = new google.maps.places.Autocomplete(
+            (document.getElementById("newLocation")),
+            { types: "['geocode', 'address', 'establishments' ]" }
+        );
+    };
+    $scope.change = function() {
+        let place = this.getPlace();
+        $scope.lat = place.geometry.location.lat();
+        $scope.lng = place.geometry.location.lng();
+    };
 });
