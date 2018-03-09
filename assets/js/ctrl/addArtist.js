5800e50f30c449df3f33dfa0a8808a1e53fe313d
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-09 15:26:08 -0600

    adding artist works, no form validation

--- assets/js/ctrl/addArtist.js
+++ assets/js/ctrl/addArtist.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module("inkmap").controller("AddArtistCtrl", function ($scope, GOOGLE) {
+angular.module("inkmap").controller("AddArtistCtrl", function ($scope, GOOGLE, ArtistFactory) {
     $scope.mapScript = `http://maps.google.com/maps/api/js?key=${GOOGLE.apiKey}&libraries=places&callback=initialize`;
 
     let autocomplete;
@@ -13,5 +13,20 @@ angular.module("inkmap").controller("AddArtistCtrl", function ($scope, GOOGLE) {
         let place = this.getPlace();
         $scope.lat = place.geometry.location.lat();
         $scope.lng = place.geometry.location.lng();
+    };
+    $scope.add = (lat, lng) => {
+        $scope.artist.loc = {lat, lng};
+        if ($scope.tags) {
+            $scope.artist.tags = $scope.tags.split(",").map(w => w.trim());
+        } else {
+            $scope.artist.tags = [];
+        }
+        ArtistFactory.addArtist($scope.artist)
+            .then(response => {
+                $scope.err = "";
+            })
+            .catch(err => {
+                $scope.err = err;
+            });
     };
 });
--- assets/js/factory/artist.js
+++ assets/js/factory/artist.js
@@ -1,6 +1,8 @@
 "use strict";
 
-angular.module("inkmap").factory("ArtistFactory", function($q, $http, FIREBASE) {
+const _ = require("lodash");
+
+angular.module("inkmap").factory("ArtistFactory", function($q, $http, FIREBASE, $location) {
     const getArtists = () => {
         return $q((resolve, reject) => {
             $http.get(`${FIREBASE.db}/artists.json`)
@@ -9,5 +11,34 @@ angular.module("inkmap").factory("ArtistFactory", function($q, $http, FIREBASE) 
         });
     };
 
-    return {getArtists};
+    const addArtist = ({name, email, tags, loc, instagram}) => {
+        return $q((resolve, reject) => {
+            searchArtists("name", name)
+                .then(artists => {
+                    if (artists) {
+                        return reject("Duplicate artist.");
+                    } else {
+                        $http.post(`${FIREBASE.db}/artists.json`,{name, email, tags, loc, instagram})
+                            .then(response => {
+                                if (response.status == 200) {
+                                    $location.path("/");
+                                }
+                            })
+                            .catch(err => console.log(err));
+                    }
+                })
+                .catch(err => console.log(err));
+        });
+    };
+
+    const searchArtists = (property, term) => {
+        return $q((resolve, reject) => {
+            getArtists()
+                .then(artists => {
+                    resolve(_.find(artists, [property, term]));
+                });
+        });
+    };
+
+    return {getArtists, addArtist};
 });
